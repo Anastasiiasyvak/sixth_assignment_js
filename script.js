594b595f45c1e8a49e8de2e368afcc615afafd1f
Anastasiiasyvak <asyvak@kse.org.ua>
2023-09-27 20:35:48 +0300

    ReversedString

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function reverse{
-    const str = "Geeks for Geeks";
-    const reversedString = str.split("").reduce((acc, char) => char + acc, "");
+function reverseString() {
+    var input = document.getElementById('input').value;
+    var result = document.getElementById('result');
+    var reversedInput = input.split('').reverse().join('');
+    result.textContent = reversedInput;
 }
+
+
+var timeoutID;
+
+function text_one_second() {
+    timeoutID = window.setTimeout(reverseString, 1000);
+  }
+
+var inputElement = document.getElementById('input');
+inputElement.addEventListener('input', text_one_second);
